960bc17de7e145efd90522d7346519285cc09901
Sam Stern <hatboysam@gmail.com>
2012-07-31 12:42:51 -0400

    Resolved Conflicts

--- src/com/taptag/beta/vendor/Vendor.java
+++ src/com/taptag/beta/vendor/Vendor.java
@@ -1,15 +1,48 @@
 package com.taptag.beta.vendor;
 
 import java.io.Serializable;
-
+import org.codehaus.jackson.map.annotate.JsonRootName;
 import com.taptag.beta.location.TagAddress;
 
 @SuppressWarnings("serial")
+@JsonRootName(value = "vendor")
 public class Vendor implements Comparable<Vendor>, Serializable {
-	
+
+	private int id;
 	private String name;
+	private int company_id;
 	private TagAddress address;
-	
+
+	// //private String category;
+
+	public Vendor() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCompanyId() {
+		return company_id;
+	}
+
+	public void setCompany_id(int company_id) {
+		this.company_id = company_id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAddress(TagAddress address) {
+		this.address = address;
+	}
+
 	public Vendor(String name, TagAddress address) {
 		this.name = name;
 		this.address = address;
@@ -23,11 +56,9 @@ public class Vendor implements Comparable<Vendor>, Serializable {
 		return address;
 	}
 
-	@Override
 	public int compareTo(Vendor arg0) {
 		String thisName = this.getName();
 		String otherName = arg0.getName();
 		return thisName.compareTo(otherName);
 	}
-
 }
